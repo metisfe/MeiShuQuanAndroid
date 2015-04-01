f278138ef0360b1b0f36e610a5dd3b4896559e6e
Lechao Diwu <wudi@microsoft.com>
2015-04-02 02:51:11 +0800

    post moment

--- app/src/main/java/com/metis/meishuquan/fragment/main/CircleFragment.java
+++ app/src/main/java/com/metis/meishuquan/fragment/main/CircleFragment.java
@@ -2,6 +2,8 @@ package com.metis.meishuquan.fragment.main;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +11,8 @@ import android.view.ViewGroup;
 import com.metis.meishuquan.MainApplication;
 import com.metis.meishuquan.R;
 import com.metis.meishuquan.fragment.BaseFragment;
+import com.metis.meishuquan.fragment.Topline.ItemInfoFragment;
+import com.metis.meishuquan.fragment.circle.PostMomentFragment;
 import com.metis.meishuquan.view.circle.CircleTitleBar;
 import com.metis.meishuquan.view.shared.TabBar;
 
@@ -28,7 +32,21 @@ public class CircleFragment extends BaseFragment {
         this.tabBar.setTabSelectedListener(MainApplication.MainActivity);
         this.titleBar = (CircleTitleBar) rootView.findViewById(R.id.fragment_shared_circlefragment_title_bar);
         titleBar.setText("user name");
-        titleBar.setRight("post",null);
+        titleBar.setRight("post",new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PostMomentFragment postMomentFragment = new PostMomentFragment();
+                Bundle args = new Bundle();
+                postMomentFragment.setArguments(args);
+
+                FragmentManager fm = getActivity().getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.setCustomAnimations(R.anim.fragment_in, R.anim.fragment_out);
+                ft.add(R.id.content_container, postMomentFragment);
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
         return rootView;
     }
 }
