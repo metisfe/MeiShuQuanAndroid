f00a6261046559c8ed9fec6a31daa6b205808fa2
wangjin1613 <287826481@qq.com>
2015-07-15 19:17:11 +0800

    更换Tabbar 圈子和课程位置

--- app/src/main/java/com/metis/meishuquan/view/shared/TabBar.java
+++ app/src/main/java/com/metis/meishuquan/view/shared/TabBar.java
@@ -26,26 +26,26 @@ public class TabBar extends LinearLayout implements View.OnClickListener {
         public void onTabSelected(SelectedTabType type);
     }
 
-    //头条
     private int tabbarType;
-    private View followeesTab;
-    private ImageView followeesIcon;
-    private TextView followeesTitle;
+    //头条
+    private View toplineTab;
+    private ImageView toplineIcon;
+    private TextView toplineTitle;
 
     //点评
-    private static View topStoryTab;
-    private ImageView topStoryIcon;
-    private TextView topStoryTitle;
+    private static View commentTab;
+    private ImageView commentIcon;
+    private TextView commentTitle;
 
     //circle
     private View circleTab;
     private ImageView circleIcon;
-//    private TextView circleTitle;
+    private TextView circleTitle;
 
     //课程
-    private static View discoverTab;
-    private ImageView discoverIcon;
-    private TextView discoverTitle;
+    private static View centerTab;
+    private ImageView centerIcon;
+//    private TextView discoverTitle;
 
     //我
     private View activityTab;
@@ -83,19 +83,19 @@ public class TabBar extends LinearLayout implements View.OnClickListener {
         if (this.tabSelectedListener != null) {
             SelectedTabType type = null;
             switch (v.getId()) {
-                case R.id.view_shared_tabbar_followees:
+                case R.id.view_tabbar_topLine:
                     type = SelectedTabType.TopLine;
                     break;
-                case R.id.view_shared_tabbar_topstory:
+                case R.id.view_tabbar_comment:
                     type = SelectedTabType.Comment;
                     break;
-                case R.id.view_shared_tabbar_discover:
+                case R.id.view_tabbar_course:
                     type = SelectedTabType.Class;
                     break;
-                case R.id.view_shared_tabbar_activity:
+                case R.id.view_tabbar_myinfo:
                     type = SelectedTabType.MyInfo;
                     break;
-                case R.id.view_shared_tabbar_circle:
+                case R.id.view_tabbar_circle:
                     type = SelectedTabType.Circle;
                     break;
             }
@@ -111,25 +111,25 @@ public class TabBar extends LinearLayout implements View.OnClickListener {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.view_shared_tabbar, this);
 
-        this.followeesTab = this.findViewById(R.id.view_shared_tabbar_followees);
-        this.followeesIcon = (ImageView) this.findViewById(R.id.view_shared_tabbar_followees_icon);
-        this.followeesTitle = (TextView) this.findViewById(R.id.view_shared_tabbar_followees_title);
-        this.topStoryTab = this.findViewById(R.id.view_shared_tabbar_topstory);
-        this.topStoryIcon = (ImageView) this.findViewById(R.id.view_shared_tabbar_topstory_icon);
-        this.topStoryTitle = (TextView) this.findViewById(R.id.view_shared_tabbar_topstory_title);
-        this.discoverTab = this.findViewById(R.id.view_shared_tabbar_discover);
-        this.discoverIcon = (ImageView) this.findViewById(R.id.view_shared_tabbar_discover_icon);
-        this.discoverTitle = (TextView) this.findViewById(R.id.view_shared_tabbar_discover_title);
-        this.activityTab = this.findViewById(R.id.view_shared_tabbar_activity);
+        this.toplineTab = this.findViewById(R.id.view_tabbar_topLine);
+        this.toplineIcon = (ImageView) this.findViewById(R.id.view_shared_tabbar_followees_icon);
+        this.toplineTitle = (TextView) this.findViewById(R.id.view_shared_tabbar_followees_title);
+        this.commentTab = this.findViewById(R.id.view_tabbar_comment);
+        this.commentIcon = (ImageView) this.findViewById(R.id.view_shared_tabbar_topstory_icon);
+        this.commentTitle = (TextView) this.findViewById(R.id.view_shared_tabbar_topstory_title);
+        this.centerTab = this.findViewById(R.id.view_tabbar_course);
+        this.centerIcon = (ImageView) this.findViewById(R.id.view_shared_tabbar_discover_icon);
+//        this.discoverTitle = (TextView) this.findViewById(R.id.view_shared_tabbar_discover_title);
+        this.activityTab = this.findViewById(R.id.view_tabbar_myinfo);
         this.activityIcon = (ImageView) this.findViewById(R.id.view_shared_tabbar_activity_icon);
         this.activityTitle = (TextView) this.findViewById(R.id.view_shared_tabbar_activity_title);
         this.activityTip = (TextView) this.findViewById(R.id.view_shared_tabbar_activity_tip);
-        this.circleTab = this.findViewById(R.id.view_shared_tabbar_circle);
+        this.circleTab = this.findViewById(R.id.view_tabbar_circle);
         this.circleIcon = (ImageView) this.findViewById(R.id.view_shared_tabbar_circle_icon);
-//        this.circleTitle = (TextView) this.findViewById(R.id.view_shared_tabbar_circle_title);
-        this.followeesTab.setOnClickListener(this);
-        this.topStoryTab.setOnClickListener(this);
-        this.discoverTab.setOnClickListener(this);
+        this.circleTitle = (TextView) this.findViewById(R.id.view_shared_tabbar_circle_title);
+        this.toplineTab.setOnClickListener(this);
+        this.commentTab.setOnClickListener(this);
+        this.centerTab.setOnClickListener(this);
         this.activityTab.setOnClickListener(this);
         this.circleTab.setOnClickListener(this);
 
@@ -157,15 +157,15 @@ public class TabBar extends LinearLayout implements View.OnClickListener {
     public static void showOrHide(int index, boolean isShow) {
         if (index == 3) {
             if (isShow) {
-                discoverTab.setVisibility(View.VISIBLE);
+                centerTab.setVisibility(View.VISIBLE);
             } else {
-                discoverTab.setVisibility(View.GONE);
+                centerTab.setVisibility(View.GONE);
             }
         } else if (index == 1) {
             if (isShow) {
-                topStoryTab.setVisibility(View.VISIBLE);
+                commentTab.setVisibility(View.VISIBLE);
             } else {
-                topStoryTab.setVisibility(View.GONE);
+                commentTab.setVisibility(View.GONE);
             }
         }
     }
@@ -193,15 +193,15 @@ public class TabBar extends LinearLayout implements View.OnClickListener {
      * @param isSelected 是否选中
      */
     public void selectedFolloweesTab(boolean isSelected) {
-        this.followeesTitle.setText(R.string.tab_topLine);
+        this.toplineTitle.setText(R.string.tab_topLine);
         if (isSelected)//选中
         {
-            this.followeesIcon.setImageResource(R.drawable.icon_tabbar_top_selected);
-            this.followeesTitle.setTextColor(this.getResources().getColor(R.color.view_shared_tab_bar_selected_title_color));
+            this.toplineIcon.setImageResource(R.drawable.icon_tabbar_top_selected);
+            this.toplineTitle.setTextColor(this.getResources().getColor(R.color.view_shared_tab_bar_selected_title_color));
         } else//未选中
         {
-            this.followeesIcon.setImageResource(R.drawable.icon_tabbar_top_unselected);
-            this.followeesTitle.setTextColor(this.getResources().getColor(R.color.view_shared_tab_bar_unselected_title_color));
+            this.toplineIcon.setImageResource(R.drawable.icon_tabbar_top_unselected);
+            this.toplineTitle.setTextColor(this.getResources().getColor(R.color.view_shared_tab_bar_unselected_title_color));
         }
     }
 
@@ -211,13 +211,13 @@ public class TabBar extends LinearLayout implements View.OnClickListener {
      * @param isSelected 选中状态
      */
     public void selectedTopStoryTab(boolean isSelected) {
-        this.topStoryTitle.setText(R.string.tab_comment);
+        this.commentTitle.setText(R.string.tab_comment);
         if (isSelected) {
-            this.topStoryIcon.setImageResource(R.drawable.icon_tabbar_assess_selected);
-            this.topStoryTitle.setTextColor(this.getResources().getColor(R.color.view_shared_tab_bar_selected_title_color));
+            this.commentIcon.setImageResource(R.drawable.icon_tabbar_assess_selected);
+            this.commentTitle.setTextColor(this.getResources().getColor(R.color.view_shared_tab_bar_selected_title_color));
         } else {
-            this.topStoryIcon.setImageResource(R.drawable.icon_tabbar_assess_unselected);
-            this.topStoryTitle.setTextColor(this.getResources().getColor(R.color.view_shared_tab_bar_unselected_title_color));
+            this.commentIcon.setImageResource(R.drawable.icon_tabbar_assess_unselected);
+            this.commentTitle.setTextColor(this.getResources().getColor(R.color.view_shared_tab_bar_unselected_title_color));
         }
     }
 
@@ -227,13 +227,13 @@ public class TabBar extends LinearLayout implements View.OnClickListener {
      * @param isSelected 选中状态
      */
     public void selectedDiscoverTab(boolean isSelected) {
-        this.discoverTitle.setText(R.string.tab_course);
+//        this.discoverTitle.setText(R.string.tab_course);
         if (isSelected) {
-            this.discoverIcon.setImageResource(R.drawable.icon_tabbar_course_selected);
-            this.discoverTitle.setTextColor(this.getResources().getColor(R.color.view_shared_tab_bar_selected_title_color));
+            this.centerIcon.setImageResource(R.drawable.icon_tabbar_course);
+//            this.discoverTitle.setTextColor(this.getResources().getColor(R.color.view_shared_tab_bar_selected_title_color));
         } else {
-            this.discoverIcon.setImageResource(R.drawable.icon_tabbar_course_unselected);
-            this.discoverTitle.setTextColor(this.getResources().getColor(R.color.view_shared_tab_bar_unselected_title_color));
+            this.centerIcon.setImageResource(R.drawable.icon_tabbar_course);
+//            this.discoverTitle.setTextColor(this.getResources().getColor(R.color.view_shared_tab_bar_unselected_title_color));
         }
     }
 
@@ -259,13 +259,13 @@ public class TabBar extends LinearLayout implements View.OnClickListener {
      * @param isSelected 选中状态
      */
     public void selectedCircleTab(boolean isSelected) {
-//        this.circleTitle.setText(R.string.tab_circle);
+        this.circleTitle.setText(R.string.tab_circle);
         if (isSelected) {
-            this.circleIcon.setImageResource(R.drawable.icon_tabbar_circle_selected2);
-//            this.circleTitle.setTextColor(this.getResources().getColor(R.color.view_shared_tab_bar_selected_title_color));
+            this.circleIcon.setImageResource(R.drawable.icon_tabbar_circle_selected3);
+            this.circleTitle.setTextColor(this.getResources().getColor(R.color.view_shared_tab_bar_selected_title_color));
         } else {
-            this.circleIcon.setImageResource(R.drawable.icon_tabbar_circle_selected2);
-//            this.circleTitle.setTextColor(this.getResources().getColor(R.color.view_shared_tab_bar_unselected_title_color));
+            this.circleIcon.setImageResource(R.drawable.icon_tabbar_circle_unselected3);
+            this.circleTitle.setTextColor(this.getResources().getColor(R.color.view_shared_tab_bar_unselected_title_color));
         }
     }
 }
